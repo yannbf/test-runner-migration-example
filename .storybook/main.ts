35b446fad6256749fce11f18e0496bc0fcdeaaa4
Yann Braga <yannbf@gmail.com>
2025-10-28 11:13:13 +0100

    add storybook tests to CI

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ const config: StorybookConfig = {
   "stories": [
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": ["@storybook/addon-a11y"],
+  "addons": ["@storybook/addon-a11y", "@storybook/addon-coverage"],
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
